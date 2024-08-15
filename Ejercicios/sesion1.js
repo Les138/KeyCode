82d8d77545d162a4a0236a4ac374ab7731ed2827
Les138 <138824809+Les138@users.noreply.github.com>
2024-08-14 19:33:05 -0500

    Creacion del Repositorio

---
+++ Ejercicios/sesion1.js
@@ -0,0 +1,21 @@
+//Let
+let edadUsuario = 25;
+console.log("let", edadUsuario)
+edadUsuario = 26;
+console.log("let", edadUsuario)
+
+//Const
+const nombreUsuario = "Maria"
+console.log("nombre: ", nombreUsuario)
+
+//Condicionales
+const edadMinima = 18
+const usuario = {'nombre': 'Maria'}
+
+if (condition) {
+    
+} else {
+    
+}
+
+//condition ? 
