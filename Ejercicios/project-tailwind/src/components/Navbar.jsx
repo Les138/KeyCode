541746dc75972a1d3953f1e51a5fe8ea2a409a55
Les138 <138824809+Les138@users.noreply.github.com>
2024-09-08 20:44:31 -0500

    Tailwind

---
+++ Ejercicios/project-tailwind/src/components/Navbar.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [estaAbierto, setEstaAbierto] = useState(false);
+
+  const menuCelular = () => {
+    setEstaAbierto(!estaAbierto);
+  };
+
+  return (
+    /** la clase myNav esta en index.css pero es tailwind! */
+    <nav className="myNav">
+      <div className="container flex items-center justify-between">
+        {/* //My Logo// */}
+        <div className="text-2xl font-bold">MyLogo</div>
+        {/* //Botones// */}
+        <div className="hidden space-x-4 md:flex">
+          <button className="p-1 text-black bg-white border rounded hover:text-gray-200 hover:bg-black ">
+            Inicio
+          </button>
+          <button className="p-1 text-black bg-white border rounded hover:text-gray-200 hover:bg-black ">
+            Servicios
+          </button>
+          <button className="p-1 text-black bg-white border rounded hover:text-gray-200 hover:bg-black ">
+            Nosotros
+          </button>
+          <button className="p-1 text-black bg-white border rounded hover:text-gray-200 hover:bg-black ">
+            PQR
+          </button>
+          <button className="p-1 text-black bg-white border rounded hover:text-gray-200 hover:bg-black ">
+            Contactos
+          </button>
+          <button className="p-1 text-black bg-white border rounded hover:text-gray-200 hover:bg-black ">
+            Ubicacion
+          </button>
+        </div>
+        {/* //Usuario// */}
+        <div className="hidden p-2 text-2xl border md:flex rounded-2xl">
+          <i class="fa-solid fa-user-tie"></i>
+        </div>
+        {/* //Menu Hamburguesa// */}
+        <div className="text-2xl md:hidden">
+          <button onClick={menuCelular}>
+            <i class="fa-solid fa-ellipsis"></i>
+          </button>
+        </div>
+
+        {estaAbierto && (
+          <div className="absolute left-0 w-full text-center bg-blue-600 md:hidden top-16">
+            <button className="block w-full p-1 py-2 rounded hover:text-gray-200">
+              Inicio
+            </button>
+            <button className="block w-full p-1 py-2 rounded hover:text-gray-200">
+              Servicios
+            </button>
+            <button className="block w-full p-1 py-2 rounded hover:text-gray-200">
+              Nosotros
+            </button>
+            <button className="block w-full p-1 py-2 rounded hover:text-gray-200">
+              PQR
+            </button>
+            <button className="block w-full p-1 py-2 rounded hover:text-gray-200">
+              Contactos
+            </button>
+            <button className="block w-full p-1 py-2 rounded hover:text-gray-200">
+              Ubicacion
+            </button>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
