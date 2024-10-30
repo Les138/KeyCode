2bc3bfc079340113b6a9115212d2d251be8578ef
Les138 <138824809+Les138@users.noreply.github.com>
2024-10-29 21:35:02 -0500

    cambio

--- Hakatoon/Reto1/src/descifrar.js
+++ Hakatoon/Reto1/src/descifrar.js
@@ -1,38 +1,40 @@
-export const mensaje = "^[()`~^*/?`[()^+`-~()#[$()/~()%\\[]()}+[()[{=~¿()=$?%?!?]¡~()¿()`¬^~()[{=~()?+^[]=?()^\\{()*~¡[$[{()¡[()?=[]`\\¬]()¿()[{;+[$_~()`~]=\\]+~";
+export const mensaje =
+  "^[()`~^*/?`[()^+`-~()#[$()/~()%\\[]()}+[()[{=~¿()=$?%?!?]¡~()¿()`¬^~()[{=~()?+^[]=?()^\\{()*~¡[$[{()¡[()?=[]`\\¬]()¿()[{;+[$_~()`~]=\\]+~";
 
 // Diccionario de sustitución
 const descifrarMensaje = {
-    '()': " ",
-    '?': 'a',
-    '[': 'e', 
-    '\\': 'i',
-    '~': 'o', 
-    '+': 'u',
-    '¬': 'ó',
-    '¿': 'y',
-    '}': 'q',
-    '^': 'm',
-    '`': 'c',
-    '/': 'l',
-    '*': 'p',
-    '%': 'b',
-    ']': 'n',
-    '-': 'h',
-    '#': 'v',
-    '$': 'r',
-    '{': 's',
-    '=': 't',
-    '!': 'j',
-    '¡': 'd',
-    ';': 'f',
-    '_': 'z',
+  "()": " ",
+  "?": "a",
+  "[": "e",
+  "\\": "i",
+  "~": "o",
+  "+": "u",
+  "¬": "ó",
+  "¿": "y",
+  "}": "q",
+  "^": "m",
+  "`": "c",
+  "/": "l",
+  "*": "p",
+  "%": "b",
+  "]": "n",
+  "-": "h",
+  "#": "v",
+  $: "r",
+  "{": "s",
+  "=": "t",
+  "!": "j",
+  "¡": "d",
+  ";": "f",
+  _: "z",
 };
 
 // Función para realizar las sustituciones
 export function descifrar(text) {
-    let result = text.replace(/\(\)/g, descifrarMensaje['()']);
-    result = result.split('').map(char => descifrarMensaje[char] || char).join('');
-    return result;
+  let result = text.replace(/\(\)/g, descifrarMensaje["()"]);
+  result = result
+    .split("")
+    .map((char) => descifrarMensaje[char] || char)
+    .join("");
+  return result;
 }
-
-console.log(descifrar(mensaje))
